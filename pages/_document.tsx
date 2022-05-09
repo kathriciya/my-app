c33e826b676bcb91d059edbed943e5679edd5290
Ekaterina <Kata_Yurevna@mail.ru>
2022-05-09 21:55:04 +0700

    First commit

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps }  from "next/document";
+
+class MyDocument extends Document {
+	static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+		const initinalProps= await Document.getInitialProps(ctx);
+		return {...initinalProps};
+	}
+	render(){
+		return (
+			<Html lang='ru'>
+				<Head/>
+				{/* <link rel="preconnect" href="https://fonts.googleapis.com"/> */}
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet"/>
+				<body>
+					<Main/>
+					<NextScript/>
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      hghghg
+    </div>
+  );
+}
