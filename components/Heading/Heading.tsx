131fdcd081e3a330d4956b1fb875b1bbebed9cdb
Ekaterina <Kata_Yurevna@mail.ru>
2022-05-13 11:07:40 +0700

    update

--- components/Heading/Heading.tsx
+++ components/Heading/Heading.tsx
@@ -1,7 +1,7 @@
 import { HeadingProps } from "./Heading.props";
 import styles from './Heading.module.css';
 
-export const Heading =({tag, children }: HeadingProps)=>{
+export const Heading =({tag, children }: HeadingProps): JSX.Element =>{
   switch(tag) {
     case 'h1':
     return <h1 className={styles.h1}>{children}</h1>;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import { Heading } from "../components/Heading/Heading";
 
 export default function Home(): JSX.Element {
   return (
-    <div>
+    <>
       <Heading tag='h1'>Текст</Heading>
-    </div>
+    </>
   );
 }
