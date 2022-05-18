44cefc88220dac1b4eb94103b8fa37667adf2252
Ekaterina <Kata_Yurevna@mail.ru>
2022-05-18 14:51:15 +0700

    add fn for correct display price

--- components/HhData/HhData.tsx
+++ components/HhData/HhData.tsx
@@ -3,6 +3,7 @@ import styles from './HhData.module.css';
 import { Card } from "../Card/Card";
 import RateIcon from './rate.svg';
 import { HhDataProps } from './HhData.props';
+import { priceRu } from '../../helpers/helpers';
 
 export const HhData =({ count, juniorSalary, middleSalary, seniorSalary  }: HhDataProps): JSX.Element =>{
     return (<div className={styles.hh}>
@@ -13,17 +14,17 @@ export const HhData =({ count, juniorSalary, middleSalary, seniorSalary  }: HhDa
         <Card className={styles.salary}>
           <div>
             <div className={styles.title}>Начальный</div>
-            <div className={styles.salaryValue}>{juniorSalary}</div>
+            <div className={styles.salaryValue}>{juniorSalary && priceRu(juniorSalary)}</div>
             <div className={styles.rate}><RateIcon className={styles.filled}/><RateIcon/><RateIcon/></div>
           </div>
           <div>
             <div className={styles.title}>Средний</div>
-            <div className={styles.salaryValue}>{middleSalary}</div>
+            <div className={styles.salaryValue}>{middleSalary && priceRu(middleSalary)}</div>
             <div className={styles.rate}><RateIcon className={styles.filled}/><RateIcon className={styles.filled}/><RateIcon/></div>
           </div>
           <div>
             <div className={styles.title}>Профессионал</div>
-            <div className={styles.salaryValue}>{seniorSalary}</div>
+            <div className={styles.salaryValue}>{seniorSalary && priceRu(seniorSalary)}</div>
             <div className={styles.rate}><RateIcon className={styles.filled}/><RateIcon className={styles.filled}/><RateIcon className={styles.filled}/></div>
           </div>
         </Card>
