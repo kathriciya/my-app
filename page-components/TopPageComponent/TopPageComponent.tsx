9c1c339643d933b2bad946a53c24b86d8c15efcf
Ekaterina <Kata_Yurevna@mail.ru>
2022-05-18 15:49:00 +0700

    display advantages, seotext and tags

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -6,6 +6,8 @@ import { Heading } from '../../components/Heading/Heading';
 import { Tag } from '../../components/Tag/Tag';
 import { HhData } from '../../components/HhData/HhData';
 import { TopLevelCategory } from '../../interfaces/page.interface';
+import { Advantages } from '../../components/Advantages/Advantages';
+import { Paragraph } from '../../components/Paragraph/Paragraph';
 
 export const TopPageComponent =({page, products, firstCategory }: TopPageComponentProps): JSX.Element =>{
     return (
@@ -22,7 +24,15 @@ export const TopPageComponent =({page, products, firstCategory }: TopPageCompone
         <Heading tag='h2'>Вакансии - {page.category}</Heading>
         <Tag color='red' size='m'>hh.ru</Tag>
       </div>
-      {firstCategory == TopLevelCategory.Courses && <HhData {...page.hh}/>}
+      {firstCategory == TopLevelCategory.Courses && page.hh && <HhData {...page.hh}/>}
+      {page.advantages && page.advantages.length > 0 && <>
+				<Heading tag='h2'>Преимущства</Heading>
+				<Advantages advantages={page.advantages} />
+			</>
+			}
+      {page.seoText && <Paragraph>{page.seoText}</Paragraph>}
+      <Heading tag='h2'>Получаемые навыки</Heading>
+			{page.tags.map(t => <Tag key={t} color='primary'>{t}</Tag>)}
       </div>
       
     );
