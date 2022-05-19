b58bbc3eec601d9c08e798c9f182b963b0bfd438
Ekaterina <Kata_Yurevna@mail.ru>
2022-05-20 00:40:55 +0700

    create "Divider"

---
+++ components/Divider/Divider.props.ts
@@ -0,0 +1,5 @@
+  import { DetailedHTMLProps, HTMLAttributes } from 'react';
+
+type DividerType = DetailedHTMLProps<HTMLAttributes<HTMLHRElement>, HTMLHRElement>;
+
+export interface DividerProps extends  DividerType {}
