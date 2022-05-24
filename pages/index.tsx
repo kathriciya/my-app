01796d1ca4328659d5b75afa2170186424145f6a
Ekaterina <Kata_Yurevna@mail.ru>
2022-05-24 13:35:35 +0700

    create api

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,30 +7,42 @@ import { Rating } from "../components/Rating/Rating";
 import { Tag } from "../components/Tag/Tag";
 import { withLayout } from "../layout/Layout";
 // import { Layout } from "../layout/Layout";
-import axios from 'axios';
+import axios from "axios";
 import { MenuItem } from "../interfaces/menu.interface";
 import { Input } from "../components/Input/Input";
 import { Textarea } from "../components/Textarea/Textarea";
+import { API } from "../helpers/api";
 
-
-function Home({menu}: HomeProps): JSX.Element {
-  const [rating, setRating]=useState<number>(4);
+function Home({ menu }: HomeProps): JSX.Element {
+  const [rating, setRating] = useState<number>(4);
   return (
     <>
-      <Heading tag='h1'>Текст</Heading>
-      <Button appearance='primary' arrow='right'>Кнопка</Button>
-      <Button appearance='ghost' arrow="down">Кнопка</Button>
+      <Heading tag="h1">Текст</Heading>
+      <Button appearance="primary" arrow="right">
+        Кнопка
+      </Button>
+      <Button appearance="ghost" arrow="down">
+        Кнопка
+      </Button>
       <Paragraph size="l">Большой</Paragraph>
       <Paragraph size="m">Средний</Paragraph>
       <Paragraph size="s">Маленький</Paragraph>
-      <Tag size='s'>Ghost</Tag>
-      <Tag size='m' color='red'>Red</Tag>
-      <Tag size='s' color='green'>Green</Tag>
-      <Tag size='m' color='grey' >Grey</Tag>
-      <Tag size='s' color='primary'>Primary</Tag>
-      <Rating rating={rating} isEditable setRating={setRating}/>
-      <Input placeholder="jfdhg"/>
-      <Textarea placeholder="jghfghfdhg"/>
+      <Tag size="s">Ghost</Tag>
+      <Tag size="m" color="red">
+        Red
+      </Tag>
+      <Tag size="s" color="green">
+        Green
+      </Tag>
+      <Tag size="m" color="grey">
+        Grey
+      </Tag>
+      <Tag size="s" color="primary">
+        Primary
+      </Tag>
+      <Rating rating={rating} isEditable setRating={setRating} />
+      <Input placeholder="jfdhg" />
+      <Textarea placeholder="jghfghfdhg" />
     </>
   );
 }
@@ -38,19 +50,19 @@ function Home({menu}: HomeProps): JSX.Element {
 export default withLayout(Home);
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
-  const firstCategory=0;
-  const {data: menu} = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
-    firstCategory
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
+    firstCategory,
   });
   return {
     props: {
       menu,
-      firstCategory
-    }
+      firstCategory,
+    },
   };
 };
 
-interface HomeProps extends Record<string, unknown>{
+interface HomeProps extends Record<string, unknown> {
   menu: MenuItem[];
   firstCategory: number;
 }
--- pages/search.tsx
+++ pages/search.tsx
@@ -1,33 +1,29 @@
-import { GetStaticProps } from 'next';
-import { withLayout } from '../layout/Layout';
-import axios from 'axios';
-import { MenuItem } from '../interfaces/menu.interface';
+import { GetStaticProps } from "next";
+import { withLayout } from "../layout/Layout";
+import axios from "axios";
+import { MenuItem } from "../interfaces/menu.interface";
+import { API } from "../helpers/api";
 
 function Search(): JSX.Element {
-
-	return (
-		<>
-			Search
-		</>
-	);
+  return <>Search</>;
 }
 
 export default withLayout(Search);
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
-	const firstCategory = 0;
-	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
-		firstCategory
-	});
-	return {
-		props: {
-			menu,
-			firstCategory
-		}
-	};
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
+    firstCategory,
+  });
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
 };
 
 interface HomeProps extends Record<string, unknown> {
-	menu: MenuItem[];
-	firstCategory: number;
+  menu: MenuItem[];
+  firstCategory: number;
 }
