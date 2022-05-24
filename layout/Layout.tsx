194ec93e91f342fd5f3bcf14a89b5b4abe916d2a
Ekaterina <Kata_Yurevna@mail.ru>
2022-05-25 01:25:08 +0700

    display "Up"

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,32 +1,34 @@
 import { LayoutProps } from "./Layout.props";
-import styles from './Layout.module.css';
+import styles from "./Layout.module.css";
 // import cn from 'classnames';
 import { Header } from "./Header/Header";
 import { Sidebar } from "./Sidebar/Sidebar";
 import { Footer } from "./Footer/Footer";
 import { FunctionComponent } from "react";
 import { AppContextProvider, IAppContext } from "../context/app.context";
+import { Up } from "../components/Up/Up";
 
-const Layout =({children}: LayoutProps): JSX.Element =>{
+const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
     <div className={styles.wrapper}>
-    <Header className={styles.header}/>
-      <Sidebar className={styles.sidebar}/>
-      <div className={styles.body}>
-        {children}
-      </div>
-    <Footer className={styles.footer}/>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body}>{children}</div>
+      <Footer className={styles.footer} />
+      <Up />
     </div>
   );
 };
 
-export const withLayout = <T extends Record<string, unknown> & IAppContext> (Component: FunctionComponent<T>) =>{
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(
+  Component: FunctionComponent<T>
+) => {
   return function withLayoutComponent(props: T): JSX.Element {
     return (
       <AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
-      <Layout>
-        <Component {...props}/>
-      </Layout>
+        <Layout>
+          <Component {...props} />
+        </Layout>
       </AppContextProvider>
     );
   };
