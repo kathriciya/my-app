07afbe881f6959532a823051d09ffebf583fae58
Ekaterina <Kata_Yurevna@mail.ru>
2022-05-24 15:23:20 +0700

    add forwarRef for Card

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,10 +1,25 @@
-import { CardProps } from './Card.props';
-import styles from './Card.module.css';
-import cn from 'classnames';
+import { CardProps } from "./Card.props";
+import styles from "./Card.module.css";
+import cn from "classnames";
+import { ForwardedRef, forwardRef } from "react";
 
-export const Card =({color = 'white', children, className, ...props }: CardProps): JSX.Element =>{
-    return <div {...props}  className={cn(styles.card, className, {
-      [styles.blue]: color == 'blue',
-    })}>{children}</div>;
-  
-};
+export const Card = forwardRef(
+  (
+    { color = "white", children, className, ...props }: CardProps,
+    ref: ForwardedRef<HTMLDivElement>
+  ): JSX.Element => {
+    return (
+      <div
+        ref={ref}
+        {...props}
+        className={cn(styles.card, className, {
+          [styles.blue]: color == "blue",
+        })}
+      >
+        {children}
+      </div>
+    );
+  }
+);
+
+Card.displayName = "Card";
