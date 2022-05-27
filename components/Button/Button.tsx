fda86956fa8875c61429e9b48447838938d1ba1c
Ekaterina <Kata_Yurevna@mail.ru>
2022-05-27 12:18:31 +0700

    add animation for button

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,17 +1,35 @@
 import { ButtonProps } from "./Button.props";
-import styles from './Button.module.css';
-import cn from 'classnames';
-import ArrowIcon from './arrow.svg';
+import styles from "./Button.module.css";
+import cn from "classnames";
+import ArrowIcon from "./arrow.svg";
+import { motion } from "framer-motion";
 
-export const Button =({appearance, arrow = "none", children, className, ...props }: ButtonProps): JSX.Element =>{
+export const Button = ({
+  appearance,
+  arrow = "none",
+  children,
+  className,
+  ...props
+}: ButtonProps): JSX.Element => {
   return (
-    <button {...props} className={cn(styles.button, className, {
-      [styles.primary]: appearance == 'primary',
-      [styles.ghost]: appearance == 'ghost'
-    })}>
+    <motion.button
+      whileHover={{ scale: 1.05 }}
+      {...props}
+      className={cn(styles.button, className, {
+        [styles.primary]: appearance == "primary",
+        [styles.ghost]: appearance == "ghost",
+      })}
+    >
       {children}
-      {arrow != 'none' && <span className={cn(styles.arrow, {
-      [styles.down]: arrow == 'down'})}><ArrowIcon/></span>}
-    </button>
+      {arrow != "none" && (
+        <span
+          className={cn(styles.arrow, {
+            [styles.down]: arrow == "down",
+          })}
+        >
+          <ArrowIcon />
+        </span>
+      )}
+    </motion.button>
   );
 };
