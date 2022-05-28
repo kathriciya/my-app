eafcfd89d9d143d571d1738c2dee3fb0b1a5301e
Ekaterina <Kata_Yurevna@mail.ru>
2022-05-28 14:28:30 +0700

    fix bug

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -12,8 +12,12 @@ import { ProductModel } from "../../interfaces/product.interface";
 import { firstLevelMenu } from "../../helpers/helpers";
 import { TopPageComponent } from "../../page-components/TopPageComponent/TopPageComponent";
 import Head from "next/head";
+import { Error404 } from "../404";
 
 function TopPage({ page, products, firstCategory }: TopPageProps): JSX.Element {
+  if (!page || !products) {
+    return <Error404 />;
+  }
   return (
     <>
       <Head>
@@ -38,7 +42,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
   let paths: string[] = [];
   for (const m of firstLevelMenu) {
     const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
-      firstCategory: m.id,
+      firstCategory: TopLevelCategory.Courses,
     });
     paths = paths.concat(
       menu.flatMap((s) => s.pages.map((p) => `/${m.route}/${p.alias}`))
@@ -47,7 +51,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   return {
     paths,
-    fallback: true,
+    fallback: false,
   };
 };
 
