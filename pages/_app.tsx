80ae4b16606fc74d676731c2c6f3be84af587352
Ekaterina <Kata_Yurevna@mail.ru>
2022-05-28 12:29:48 +0700

    add yandex metrika

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import { AppProps } from "next/dist/shared/lib/router/router";
 import "../styles/globals.css";
 import Head from "next/head";
+import ym from "react-yandex-metrika";
+import { YMInitializer } from "react-yandex-metrika";
+import Router from "next/router";
+
+Router.events.on("routeChangeComplete", (url: string) => {
+  if (typeof window !== "undefined") {
+    ym("hit", url);
+  }
+});
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
   return (
@@ -8,12 +17,18 @@ function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
       <Head>
         <title>My App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://mc.yandex.ru" />
         <meta
           property="og:url"
           content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}
         />
         <meta property="og:locale" content="ru_RU" />
       </Head>
+      <YMInitializer
+        accounts={[]}
+        options={{ webvisor: true, defer: true }}
+        version="2"
+      />
       <Component {...pageProps} />
     </>
   );
